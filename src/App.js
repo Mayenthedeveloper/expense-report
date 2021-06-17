cf1ab4657915a3f89abacc0a4d9dfe226ed002c7
Mayen Akpan <neroxma@yahoo.com>
2021-06-16 18:42:03 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+import React, { Component } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1></h1>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
