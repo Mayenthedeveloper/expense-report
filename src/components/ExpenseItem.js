c2e1a48659e9ce6a53221e6d0d6bf6f507c25755
Mayen Akpan <neroxma@yahoo.com>
2021-06-17 18:32:43 -0700

    Added components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item ">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
 }
 
 export default ExpenseItem;
+
+//component respnsible for redndering the xpensiteems
